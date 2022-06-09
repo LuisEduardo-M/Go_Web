ffe316568c229ef215f7d5dbaaec09b238b65843
Luis Eduardo M <luiseduardomc019@gmail.com>
2022-06-09 18:31:03 -0400

    page handlers

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,16 @@
-package main
+package handlers
 
 import (
+	"github.com/LuisEduardo-M/Go_Web/pkg/render"
 	"net/http"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.tmpl.html")
+	render.RenderTemplate(w, "home.tmpl.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.tmpl.html")
+	render.RenderTemplate(w, "about.tmpl.html")
 }
