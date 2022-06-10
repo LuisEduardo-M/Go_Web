b46125267f171a16de41467d618998b670bcc53b
Luis Eduardo M <luiseduardomc019@gmail.com>
2022-06-09 23:22:30 -0400

    template from cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/LuisEduardo-M/Go_Web/pkg/config"
 	"github.com/LuisEduardo-M/Go_Web/pkg/handlers"
+	"github.com/LuisEduardo-M/Go_Web/pkg/render"
+	"log"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Printf("Starting application on http://localhost%s/\n", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,17 +1,84 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/LuisEduardo-M/Go_Web/pkg/config"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template: ", err)
 		return
 	}
+}
+
+// Creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.tmpl.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob("./templates/*layout.tmpl.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, err
 }
